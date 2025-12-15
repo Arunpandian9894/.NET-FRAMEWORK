2c21b35baa162c4076bfc5acac15cb768afd9726
Arunpandian9894 <717824i505@kce.ac.in>
2025-12-15 12:29:47 +0530

    Add files via upload

---
+++ task 7/task 7/task7.3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string correctLogin = "admin";
+        string correctPassword = "1234";
+
+        int attempts = 0;
+
+        while (attempts < 3)
+        {
+            Console.Write("Enter Login: ");
+            string login = Console.ReadLine();
+
+            Console.Write("Enter Password: ");
+            string password = Console.ReadLine();
+
+            if (login == correctLogin && password == correctPassword)
+            {
+                Console.WriteLine("Login successful!");
+                break;
+            }
+            else
+            {
+                attempts++;
+                Console.WriteLine("Incorrect login or password!");
+                Console.WriteLine("Attempts left: " + (3 - attempts));
+            }
+        }
+
+        if (attempts == 3)
+        {
+            Console.WriteLine("User rejected! Too many wrong attempts.");
+        }
+    }
+}
